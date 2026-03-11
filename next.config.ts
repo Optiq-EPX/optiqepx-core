e1eb5b6627f386f510b779a4cb25098b82215175
Shuvo Chandra Deb Nath <shuvochandra999@gmail.com>
2026-03-11 23:05:36 +0600

    fix: configure remote patterns for user avatars in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.googleusercontent.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        pathname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
